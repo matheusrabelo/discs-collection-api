5d4689756f2ce0cce36857d3daaabfd5def62511
Matheus Rabelo <matheusfreirerabelo@gmail.com>
2017-12-30 17:49:51 -0200

    Add allow methods

--- source/api/index.js
+++ source/api/index.js
@@ -16,6 +16,7 @@ app.use((req, res, next) => {
   res.header('Access-Control-Allow-Origin', '*');
   res.header('Access-Control-Allow-Headers',
     'Origin, X-Requested-With, Content-Type, Accept');
+  res.header('Access-Control-Allow-Methods', 'GET,HEAD,POST,PUT,DELETE');
   next();
 });
 
