6793fa9dea912cea8bb604d86d8d0f762f940756
Matheus Rabelo <matheusfreirerabelo@gmail.com>
2017-12-29 23:04:55 -0200

    Add basic middlewares

--- source/api/index.js
+++ source/api/index.js
@@ -1,8 +1,24 @@
 import express from 'express';
+import helmet from 'helmet';
+import morgan from 'morgan';
+import bodyParser from 'body-parser';
+
 import { PORT } from '../configuration';
 import router from '../routes/router';
 
 const app = express();
+
+app.use(morgan('common'));
+app.use(helmet());
+app.use(bodyParser.json());
+
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Headers',
+    'Origin, X-Requested-With, Content-Type, Accept');
+  next();
+});
+
 app.use(router);
 
 app.listen(PORT, () => console.log(`listening on ${PORT}`));
